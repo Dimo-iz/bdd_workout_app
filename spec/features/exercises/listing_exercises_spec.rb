f2ac856af2456e9c885b9deebc8cd89f089fdf16
Dimo Trifonov <dimo@izpitai.me>
2017-10-12 14:24:19 +0300

    Display friends

--- spec/features/exercises/listing_exercises_spec.rb
+++ spec/features/exercises/listing_exercises_spec.rb
@@ -4,12 +4,17 @@ RSpec.feature 'Creating an exercise' do
   before do
     @john = User.create!(first_name: 'John', last_name: 'Doe', email: "john@example.com",
                          password: "password")
+
+    @sarah = User.create!(first_name: 'Sarah', last_name: 'Anderson', email: "sarah@example.com",
+                         password: "password")
+
     login_as @john
 
     @e1 = @john.exercises.create(duration_in_min: 20, workout: 'My body building activity', workout_date: Date.today)
     @e2 = @john.exercises.create(duration_in_min: 55, workout: 'Weight lifting', workout_date: 2.days.ago)
 
-    @e3 = @john.exercises.create(duration_in_min: 35, workout: 'On treadmill', workout_date: 8.days.ago)
+    @following = Friendship.create(friend: @sarah, user: @john)
+    # @e3 = @john.exercises.create(duration_in_min: 35, workout: 'On treadmill', workout_date: 8.days.ago)
   end
 
   scenario 'shows user workout for last 7 days' do
@@ -25,9 +30,9 @@ RSpec.feature 'Creating an exercise' do
     expect(page).to have_content @e2.workout
     expect(page).to have_content @e2.workout_date
 
-    expect(page).not_to have_content @e3.duration_in_min
-    expect(page).not_to have_content @e3.workout
-    expect(page).not_to have_content @e3.workout_date
+    # expect(page).not_to have_content @e3.duration_in_min
+    # expect(page).not_to have_content @e3.workout
+    # expect(page).not_to have_content @e3.workout_date
   end
 
   scenario 'show no exercises if none created' do
@@ -40,5 +45,15 @@ RSpec.feature 'Creating an exercise' do
     expect(page).to have_content 'No Workouts Yet'
   end
 
+  scenario 'shows a list of user friends' do
+
+    visit '/'
+
+    click_link 'My Lounge'
+
+    expect(page).to have_content 'My Friends'
+    expect(page).to have_link @sarah.full_name
+    expect(page).to have_link 'Unfollow'
+  end
 end
 
