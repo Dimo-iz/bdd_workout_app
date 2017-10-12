ca63176c69912a37b9d95bd04651b40c34a1350e
Dimo Trifonov <dimo@izpitai.me>
2017-10-12 11:20:27 +0300

    Replace hardcoded in creatign exercise spec

--- spec/features/exercises/creating_exercise_spec.rb
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -18,7 +18,7 @@ RSpec.feature 'Creating an exercise' do
 
     fill_in 'Duration', with: 70
     fill_in 'Workout Details', with: 'Weight lifting'
-    fill_in 'Activity Date', with: '2016-07-26'
+    fill_in 'Activity Date', with: 3.days.ago
 
     click_button 'Create Exercise'
 
